5676d190a6d313d638b7c8e06a2f75aceede0473
hphp777 <85219925+hphp777@users.noreply.github.com>
2021-11-12 14:01:53 +0900

    original data

--- nav.php
+++ nav.php
@@ -1,6 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
+    <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
       <li class="nav-item">
         <a class="nav-link" href="Athletes.php">Athletes</a>
       </li>
